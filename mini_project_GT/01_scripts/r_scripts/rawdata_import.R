9f0e2599ee9cd8220eddfba419c4ce51acddd03d
guillaumetougas <guillaume.toug@gmail.com>
2024-09-12 01:06:57 -0400

    minor correction

--- mini_project_GT/01_scripts/r_scripts/rawdata_import.R
+++ mini_project_GT/01_scripts/r_scripts/rawdata_import.R
@@ -2,7 +2,7 @@
 
 #link for downloading the raw data from PhenObs database in 2022 
 url_raw <-  "https://idata.idiv.de/MMM/ShowMultimediaData/getFile?path=Datasets%255c3550%255crawdata_PhenObs_2022%281%29.csv"
-destfile_raw <- "00_rawdata/processeddata_PhenObs_2022.csv"  # Path to save the file
+destfile_raw <- "00_rawdata/rawdata_PhenObs_2022.csv"  # Path to save the file
 
 download.file(url_raw, destfile_raw) #Downloading file in the right directory
 
