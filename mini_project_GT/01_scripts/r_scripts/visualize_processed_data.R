2447f7cfeabbacfe06604f71142e6ce510a82d35
guillaumetougas <guillaume.toug@gmail.com>
2024-09-29 22:06:01 -0400

    Add comments to script

--- mini_project_GT/01_scripts/r_scripts/visualize_processed_data.R
+++ mini_project_GT/01_scripts/r_scripts/visualize_processed_data.R
@@ -12,12 +12,6 @@ phenobs <- read.csv("00_rawdata/processeddata_PhenObs_2022(1).csv", sep=";")
 #Set common theme for plots
 theme_set(theme_bw())
 
-table_AM <- phenobs[phenobs$Species=="Achillea millefolium",]
-
-table_AM_fl_sen_peak<- table_AM[,c(2,8,15)]
-
-write.csv(table_AM_fl_sen_peak, "03_figures/table_peak_fl_sen.csv", row.names = F)
-
 #Plotting ranges in flowering peak for all gardens 
 ggplot(phenobs,aes(Botanic_Garden,FlPeak))+
   geom_boxplot()+
@@ -26,4 +20,11 @@ ggplot(phenobs,aes(Botanic_Garden,FlPeak))+
     ylab("Day of year of flowering peak")
 
 ggsave("03_figures/botanic_garden_peakflowering.png", width = 10.40, height =6.5 )
+
+#Example for Achillea Millefolium
+table_AM <- phenobs[phenobs$Species=="Achillea millefolium",]#subset AM
+
+table_AM_fl_sen_peak<- table_AM[,c(2,8,15)]#keep variables of interest
+
+write.csv(table_AM_fl_sen_peak, "03_figures/table_peak_fl_sen.csv", row.names = F)#prepare table for manuscript
   
