63ede5cda2f9a1be9aa3acebf3dafcd9bf152da4
guillaumetougas <guillaume.toug@gmail.com>
2024-09-30 22:40:05 -0400

    Adding a chunk for copying processed data in 02_outdata

--- mini_project_GT/01_scripts/r_scripts/rawdata_import.R
+++ mini_project_GT/01_scripts/r_scripts/rawdata_import.R
@@ -14,6 +14,9 @@ download.file(url_zip, destfile_zip,method = "curl") #Downloading file in the ri
 #Unzip Zip file
 unzip("./00_rawdata/phenObs.zip", exdir = "./00_rawdata")
 
+#copy the processed data in the 02_outdata directory
+file.copy(from = "./00_rawdata/processeddata_PhenObs_2022(1).csv", to = "./02_outdata/processeddata_PhenObs_2022(1).csv")
+
 #read csv data to make sure data is readable
 phenObs_proc <- read.csv("00_rawdata/processeddata_PhenObs_2022(1).csv", sep = ";") #Processed data
 phenObs_raw <- read.csv("00_rawdata/rawdata_PhenObs_2022(1).csv", sep = ";") #Raw data
